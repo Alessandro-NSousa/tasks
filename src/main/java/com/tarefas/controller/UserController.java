e6024943fbf428ce9d6d54af0ed7a3997a841877
Alessandro N Sousa <allessandro188@gmail.com>
2025-05-07 23:32:58 -0300

    feat: refatorado api auth, removido imports e atributos que foram movidos para o service.

--- src/main/java/com/tarefas/controller/UserController.java
+++ src/main/java/com/tarefas/controller/UserController.java
@@ -1,32 +1,20 @@
 package com.tarefas.controller;
 
 import com.tarefas.dto.LoginRequestDTO;
-import com.tarefas.domain.user.User;
 import com.tarefas.dto.RegisterRequestDTO;
 import com.tarefas.dto.ResponseDTO;
-import com.tarefas.infra.security.TokenService;
-import com.tarefas.repository.UserRepository;
 import com.tarefas.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/auth")
 public class UserController {
 
-    @Autowired
-    private UserRepository repository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private TokenService tokenService;
     @Autowired
     private UserService userService;
 
