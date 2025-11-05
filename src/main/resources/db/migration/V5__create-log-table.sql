8cc3dd8d381c5fafa7f6fe4c6c8b1dfaa143e97b
Alessandro-NSousa <allessandro188@gmail.com>
2025-11-05 17:42:49 -0300

    merge: from john to develop.

--- src/main/resources/db/migration/V5__create-log-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE log (
-     id UUID PRIMARY KEY,
-     ativo BOOLEAN,
-     data_criacao TIMESTAMP,
-     descricao VARCHAR(255),
-     classe_entidade VARCHAR(255),
-     identidade UUID
-);
